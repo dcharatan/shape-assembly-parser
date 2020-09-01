7773ab1f6bcc8bf25f83f202ba176b441d637447
David Charatan <david.charatan@gmail.com>
2020-08-31 22:08:10 -0700

    Add slightly more complex test.

--- src/expression/__tests__/ExpressionParser.unit.spec.ts
+++ src/expression/__tests__/ExpressionParser.unit.spec.ts
@@ -58,6 +58,22 @@ describe('ExpressionParser Unit Tests', () => {
         ]);
         expect(expressionParser.parseExpression(given)).toEqual(expected);
       });
+
+      test('-5 * 3 / -1', () => {
+        const given = ['-', '5', '*', '3', '/', '-', '1'].map(makeToken);
+        const expected = new ExpressionNode(given[2], [
+          new ExpressionNode(given[0], [
+            new ExpressionNode(given[1], []),
+          ]),
+          new ExpressionNode(given[4], [
+            new ExpressionNode(given[3], []),
+            new ExpressionNode(given[5], [
+              new ExpressionNode(given[6], []),
+            ]),
+          ]),
+        ]);
+        expect(expressionParser.parseExpression(given)).toEqual(expected);
+      });
     });
 
     describe('unary operators', () => {
